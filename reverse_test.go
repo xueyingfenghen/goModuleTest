e7f9df12bb0d4859768aac125e2c022994ffc7ca
林望琛 <linwc@xmpaoyou.com>
2022-11-11 17:15:06 +0800

    first commit

---
+++ reverse_test.go
@@ -0,0 +1,27 @@
+package morestrings
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestReverseRunes11(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := ReverseRunes(c.in)
+		if got != c.want {
+			t.Errorf("ReverseRunes(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+	fmt.Println("======1======")
+}
+
+func TestReverseRunes22(t *testing.T) {
+	fmt.Println("======2======")
+}
