38267352df3d6af454e3fd28dd842736f69a4ebe
林望琛 <linwc@xmpaoyou.com>
2022-11-11 17:18:53 +0800

    second commit

--- reverse.go
+++ reverse.go
@@ -1,10 +1,15 @@
 package morestrings
 
+import "fmt"
+
 func ReverseRunes(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
+
+	fmt.Println("=====success=====")
+
 	return string(r)
 }
 
